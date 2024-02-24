b98f8762ef8f68ef1f4f1f5280f614e5c641b810
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-02-24 18:12:22 +0700

    Add MobileNav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
     const home_dir = "/";
@@ -8,6 +9,9 @@ const Header = () => {
                 <Link  to={home_dir} className="text-3xl font-bold tracking-tight text-orange-500">
                     MearnEats.com
                 </Link>
+                <div className="md:hidden">
+                    <MobileNav/>
+                </div>
             </div>
         </div>
     )
