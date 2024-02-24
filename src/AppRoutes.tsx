5577b879e735dd144a405bcfbbb608defe3748be
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-02-24 21:21:13 +0700

    Add responsive landing page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,14 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
 
-const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout>Home Page</Layout>} />
-      <Route path="/user-profile" element={<span>USER PROFILE</span>} />
-      <Route path="*" element={<Navigate to={""}/>} />
-    </Routes>
-  );
-};
+const AppRoutes = () => (
+  <Routes>
+    <Route
+      path="/"
+      element={<Layout>
+        <HomePage />
+      </Layout>} />
+    <Route path="/user-profile" element={<span>USER PROFILE</span>} />
+    <Route path="*" element={<Navigate to={""} />} />
+  </Routes>
+);
 
 export default AppRoutes;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <div className="bg-orange-500 py-10">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <span className="text-3xl text-white font-bold tracking-tighter">
+          MernEats.com
+        </span>
+        <span className="text-white font-bold tracking-tighter flex gap-4">
+          <span>Privacy Policy</span>
+          <span>Term of Services</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
