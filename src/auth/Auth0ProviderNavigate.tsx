41247896755a27504b269547b4eb8735f9eb506f
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-02-26 19:29:11 +0700

    Add Audience to validate JWT in backend API

--- src/auth/Auth0ProviderNavigate.tsx
+++ src/auth/Auth0ProviderNavigate.tsx
@@ -12,9 +12,10 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   // Check all .env available
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialise auth");
   }
 
@@ -28,7 +29,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
-        // audience,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
