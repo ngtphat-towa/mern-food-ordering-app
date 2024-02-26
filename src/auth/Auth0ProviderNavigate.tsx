4fd4cc96f831688f3d16249c1fbc9fc93ef1ed6b
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-02-26 10:59:39 +0700

    Wire up back end & create user API

--- src/auth/Auth0ProviderNavigate.tsx
+++ src/auth/Auth0ProviderNavigate.tsx
@@ -1,26 +1,27 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
-
-// import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  // const navigate = useNavigate();
-
+  const { createUser } = useCreateMyUser();
+  // Auth0 .ENV
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-  // const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri ) {
+  // Check all .env available
+  if (!domain || !clientId || !redirectUri) {
     throw new Error("unable to initialise auth");
   }
 
   const onRedirectCallback = (_appState?: AppState, user?: User) => {
     console.log("[Debuggin] User Info", user);
-    // navigate(appState?.returnTo || "/auth-callback");
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
 
   return (
