13c1059093da4322789cf2030762ddf46c7dbc83
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2024-02-26 18:57:45 +0700

    Create AuthCallbackPage and Sent AccessToken to backend

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,14 +1,19 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
+import AuthCallBackPage from "./pages/AuthCallBackPage";
 
 const AppRoutes = () => (
   <Routes>
     <Route
       path="/"
-      element={<Layout>
-        <HomePage />
-      </Layout>} />
+      element={
+        <Layout>
+          <HomePage />
+        </Layout>
+      }
+    />
+    <Route path="/auth-callback" element={<AuthCallBackPage />} />
     <Route path="/user-profile" element={<span>USER PROFILE</span>} />
     <Route path="*" element={<Navigate to={""} />} />
   </Routes>
--- src/auth/Auth0ProviderNavigate.tsx
+++ src/auth/Auth0ProviderNavigate.tsx
@@ -1,12 +1,13 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const { createUser } = useCreateMyUser();
+  // Navigate
+  const navigate = useNavigate();
   // Auth0 .ENV
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -17,11 +18,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("unable to initialise auth");
   }
 
-  const onRedirectCallback = (_appState?: AppState, user?: User) => {
-    console.log("[Debuggin] User Info", user);
-    if (user?.sub && user?.email) {
-      createUser({ auth0Id: user.sub, email: user.email });
-    }
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || "/auth-callback");
   };
 
   return (
